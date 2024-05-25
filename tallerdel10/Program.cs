8324fc8b0bb40cc1af7a07f422221c9ad908d04d
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2024-05-24 20:38:49 -0500

    iniciode ejercicio

---
+++ tallerdel10/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Progran
+{
+    static void Main(string[] args)
+    {
+        int N = 5; // Tamaño del tablero NxN
+        int[,] tablero = new int[N, N];
+        int filaActual = 0;
+        int columnaActual = 0;
+        int puntaje = 0;
+    }
+
