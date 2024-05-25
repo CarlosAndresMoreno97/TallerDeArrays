981b319569ad14d2185c85372ba2e9b85d2e8a22
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2024-05-24 20:41:39 -0500

    continuacion

--- tallerdel10/Program.cs
+++ tallerdel10/Program.cs
@@ -7,5 +7,18 @@
         int filaActual = 0;
         int columnaActual = 0;
         int puntaje = 0;
-    }
 
+        InicializarTablero(tablero, N);
+        MostrarTablero(tablero, filaActual, columnaActual, puntaje);
+
+        while (true)
+        {
+            ConsoleKeyInfo tecla = Console.ReadKey(true);
+
+            switch (tecla.Key)
+            {
+                case ConsoleKey.W:
+                case ConsoleKey.UpArrow:
+                    if (filaActual > 0)
+
+    }
